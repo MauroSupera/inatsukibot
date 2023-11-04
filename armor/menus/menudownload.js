eb3d00baf1204d1539e2b0efc3ca55ba86c3eebd
MauroSupera <mauroemotion@gmail.com>
2023-11-04 21:05:17 +0100

    first commit

---
+++ armor/menus/menudownload.js
@@ -0,0 +1,70 @@
+const menudownload = (Prefix, pushname, numerodn, tempo, isGroup, groupName, NomeDoBot ) => {
+
+return `
+╭━━━━━◉                                       ◉━━━━━╮
+       ╔┉💦┉═══『💧』═══┉💦┉╗    
+       ║ㅤㅤ🇩 🇴 🇼 🇳 🇱 🇴 🇦 🇩 ㅤㅤ    ║
+       ╚┉💦┉═══『💧』═══┉💦┉╝    
+╰━━━━━◉                                       ◉━━━━━╯
+ㅤㅤི⋮ ྀ💧⏝ ི⋮ ྀ  💦 ི⋮ ྀ⏝💧ི⋮ ྀ
+
+║⍆ [👤] BEM VINDO AO MENU DE DOWNLOADS
+║
+║⍆         〘 DOWNLOAD DE VÍDEOS 〙
+║⍆║ 
+║⍆║ ➲ ${Prefix}Ytmp4 [link]
+ ⍆║ ➲ ${Prefix}Ytmp44 [link]
+ ⍆║ ➲ ${Prefix}Ytmp444 [link]
+ ⍆║ ➲ ${Prefix}Ytmp4444 [link]
+ ⍆║ ➲ ${Prefix}Ytmp44444 [link]
+ ⍆║ ➲ ${Prefix}Ytmp444444 [link]
+ ⍆║ ➲ ${Prefix}Ytmp44444444 [link]
+ ⍆║ ➲ ${Prefix}Ytmp444444444 [link]
+ ⍆║ ➲ ${Prefix}Tiktok [link]
+ ⍆║ ➲ ${Prefix}Tiktokmp4 [link]
+ ⍆║ ➲ ${Prefix}Tiktokmp44 [link]
+ ⍆║ ➲ ${Prefix}Tiktokmp444 [link]
+ ⍆║ ➲ ${Prefix}Twitter [link]
+ ⍆║ ➲ ${Prefix}Twittermp4 [link]
+ ⍆║ ➲ ${Prefix}Twittermp44 [link]
+ ⍆║ ➲ ${Prefix}Twittermp444 [link]
+ ⍆║ ➲ ${Prefix}Instagram [link]
+ ⍆║ ➲ ${Prefix}Instamp4 [link]
+ ⍆║ ➲ ${Prefix}Instamp44 [link]
+ ⍆║ ➲ ${Prefix}Instamp444 [link]
+ ⍆║ ➲ ${Prefix}Facebook  [link]
+ ⍆║ ➲ ${Prefix}Facemp4 [link]
+ ⍆║ ➲ ${Prefix}Facemp44 [link]
+
+║⍆║
+║
+║⍆         〘 DOWNLOAD DE ÁUDIOS 〙
+║
+║⍆║
+║⍆║ ➲ ${Prefix}Ytmp3 [link]
+ ⍆║ ➲ ${Prefix}Ytmp33 [link]
+ ⍆║ ➲ ${Prefix}Ytmp333 [link]
+ ⍆║ ➲ ${Prefix}Ytmp3333 [link]
+ ⍆║ ➲ ${Prefix}Ytmp33333 [link]
+ ⍆║ ➲ ${Prefix}Tiktokmp3 [link]
+ ⍆║ ➲ ${Prefix}Tiktokmp33 [link]
+ ⍆║ ➲ ${Prefix}Twittermp3 [link]
+ ⍆║ ➲ ${Prefix}Twittermp33 [link]
+ ⍆║ ➲ ${Prefix}Twittermp333 [link]
+ ⍆║ ➲ ${Prefix}Instamp3 [link]
+ ⍆║ ➲ ${Prefix}Instamp33 [link]
+ ⍆║ ➲ ${Prefix}Facemp3 [link]
+ ⍆║
+║
+║⍆         〘 OUTROS DOWNLOAD 〙
+
+ ⍆║ ➲ ${Prefix}Ytdoc [link]
+ ⍆║ ➲ ${Prefix}Ytdoc2 [link]
+ ⍆║ ➲ ${Prefix}Tiktokjpg [link]
+ ⍆║ ➲ ${Prefix}Tiktokjpg2 [link]
+║
+║⍆
+╰─╼━━━══━━━≺💧≻━━━══━━━╾─╯`
+}
+
+exports.menudownload = menudownload
